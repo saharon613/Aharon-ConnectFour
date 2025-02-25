b41c0a44af0788ab990635f6ca858b7835ba1f92
Shoshana Aharon <shoshaha@Shoshanas-MacBook-Air-2.local>
2025-02-25 15:19:43 -0500

    connect four

---
+++ src/test/java/ConnectBoardTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectBoardTest {
+
+    @Test
+    void isFull() {
+        //given
+        ConnectBoard board1 = new ConnectBoard(7, 6);
+
+        //when
+        board1.insert(3, 1);
+        board1.insert(3, 2);
+        board1.insert(3, 1);
+        board1.insert(3, 2);
+        board1.insert(3, 1);
+        board1.insert(3, 2);
+
+        //then
+        assertTrue(board1.isFull(3));
+    }
+
+    @Test
+    void insert() {
+        //given
+        ConnectBoard board1 = new ConnectBoard(7, 6);
+        //when
+        board1.insert(3, 1);
+        //then
+        assertEquals(1, board1.getBoard()[5][3]);
+    }
+
+    @Test
+    void calculateWinner() {
+        ConnectBoard board1 = new ConnectBoard(7, 6);
+
+        board1.insert(3, 1);
+        board1.insert(3, 2);
+        board1.insert(3, 2);
+        board1.insert(4, 1);
+        board1.insert(5, 1);
+        board1.insert(6, 1);
+
+        assertEquals(1, board1.calculateWinner());
+    }
+}
